7a7cebd7603bf59ed578a976ee84a1d77484b96f
Fernando Sobrino <sobrino_fernando@yahoo.es>
2023-02-28 10:29:50 +0100

    "changed structure for main component - on progress"

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ const App = () => {
           </h1>
           <button><MoonIcon className="fill-red-400"/></button>
         </div>
+      </header>
         
+        
+      <main className="container mx-auto px-4 mt-8">
+
         <form className="flex gap-4 overflow-hidden rounded-md bg-white 
         py-4 px-4 items-center mt-8">
           <span className="inline-block h-5 w-5 rounded-full
@@ -23,12 +27,9 @@ const App = () => {
             placeholder="Create a new todo..." 
             className="w-full text-gray-400 outline-none"/>
         </form>
-      </header>
-
-      
-      <main className="container mx-auto px-4 mt-8">
-        <div className="rounded-md bg-white">
-          
+        
+        {/* TodoList (TodoItem) TodoUpdate y TodoDelete */}
+        <div className="rounded-md bg-white">  
           <article className="flex gap-4 py-4 px-4 border-b-gray-400 border-b">
           <button className="inline-block h-5 w-5 flex-none rounded-full
           border-2"></button>
@@ -59,18 +60,16 @@ const App = () => {
               <CrossIcon />
           </button>
         </article>
-      
-          <section className="flex justify-between py-4 px-4">
-          <span className="text-gray-400">5 items left</span>
-          <button className="text-gray-400">Clear Completed</button>
-          </section>
-          
+       
         </div>
         
-        
-        
-      </main>
+      {/* TodoComputed */}
+      <section className="flex justify-between py-4 px-4">
+      <span className="text-gray-400">5 items left</span>
+      <button className="text-gray-400">Clear Completed</button>
+      </section>
       
+      {/* TodoFilter */}
       <section className="container mx-auto mt-8 px-4 ">
         <div className="bg-white rounded-md justify-center flex gap-4">
         <button className="text-blue-600">All</button>
@@ -79,7 +78,9 @@ const App = () => {
         </div>
       </section>
 
-      <section className="text-center mt-8">Drag and drop to reorder list</section>
+      </main>
+
+      <footer className="text-center mt-8">Drag and drop to reorder list</footer>
       
     </div>
   );
